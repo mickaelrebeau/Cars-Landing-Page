031661078ad6d7d4285951db2727792b2123fe91
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-03-02 20:47:44 +0100

    💄style: update gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,28 +1,56 @@
-import { bradley, eric, mercedes, mustang, vigne } from "@/assets/images";
+import {
+	bradley,
+	eric,
+	mercedes,
+	mercedes2,
+	mustang,
+	vigne,
+} from "@/assets/images";
 import { chevrolet } from "@/assets/videos";
 
 export const Gallery = () => {
 	return (
-		<div id="Gallery" className="w-full h-auto px-8 py-12 bg-black flex flex-col items-center gap-8">
-			<div className="flex gap-4">
-				<img src={mercedes} className="w-[500px] h-[500px] object-cover" />
-				<img src={bradley} className="w-[500px] h-[500px] object-cover" />
-				<img src={eric} className="w-[800px] h-[500px] object-cover" />
+		<div
+			id="Gallery"
+			className="mx-auto w-[80%] h-auto px-8 py-12 bg-gray-950 flex flex-col items-center gap-8 rounded-md"
+		>
+			<div className="grid grid-cols-7 gap-4">
+				<img
+					src={mercedes}
+					className="w-full h-auto max-h-[415px] object-cover rounded-md col-span-2"
+				/>
+				<img
+					src={bradley}
+					className="w-full h-auto max-h-[415px] object-cover rounded-md col-span-2"
+				/>
+				<img
+					src={eric}
+					className="w-auto h-auto max-h-[415px] object-cover rounded-md col-span-3"
+				/>
 			</div>
-			<div className="flex gap-4">
+			<div className="grid grid-cols-3 gap-4">
 				<video
 					autoPlay
 					loop
 					muted
-					className="w-[1200px] h-[600px] object-cover"
+					className="w-full h-auto max-h-[500px] object-cover rounded-md  col-span-2"
 				>
 					<source src={chevrolet} type="video/mp4" />
 				</video>
-				<img src={mercedes} className="w-[600px] h-[600px] object-cover" />
+				<img
+					src={mercedes2}
+					className="w-[600px] h-auto max-h-[500px] object-cover rounded-md"
+				/>
 			</div>
-			<div className="flex gap-4">
-				<img src={vigne} className="w-[800px] h-[500px] object-cover" />
-				<img src={mustang} className="w-[1000px] h-[500px] object-cover" />
+			<div className="grid grid-cols-3 gap-4">
+				<img
+					src={vigne}
+					className="w-[800px] h-auto max-h-[500px] object-cover rounded-md"
+				/>
+				<img
+					src={mustang}
+					className="w-full h-auto max-h-[500px] object-cover rounded-md col-span-2"
+				/>
 			</div>
 		</div>
 	);
