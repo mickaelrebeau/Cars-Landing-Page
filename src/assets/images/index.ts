c2f3653e714cfe7eec6e813706607d1d2cdb6d20
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-03-02 20:46:12 +0100

    💄style: add assets

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,5 +4,7 @@ import mustang from './mustang.jpg';
 import eric from './eric.jpg';
 import phare from './phare.jpg';
 import vigne from './vigne.jpg';
+import mercedes2 from './mercedes2.jpg';
+import benz from './benz.jpg';
 
-export { bradley, mercedes, mustang, eric, phare, vigne };
+export { bradley, mercedes, mustang, eric, phare, vigne, mercedes2, benz };
