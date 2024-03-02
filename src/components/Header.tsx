e836b37eb7540ad457e677907476fe38b9142ae4
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-03-02 20:46:51 +0100

    💄style: update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,21 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
 	return (
-		<nav className="mx-auto py-8 w-[80%] flex justify-between">
-			<h1 className="text-3xl font-bold">Vintage Cars</h1>
-			<ul className="flex gap-4">
-				<li className="font-bold">
+		<nav className="mx-auto py-6 w-[90%] flex items-center justify-between">
+			<h1 className="text-3xl font-bold">
+				<Link to="/">Vintage Cars</Link>
+			</h1>
+			<ul className="flex gap-12">
+				<li className="font-semibold">
+					<a href="#Presentation">Presentation</a>
+				</li>
+				<li className="font-semibold">
 					<a href="#Gallery">Gallery</a>
 				</li>
-				<li className="font-bold">Section</li>
-				<li className="font-bold">Section</li>
-				<li className="font-bold">Section</li>
-				<li className="font-bold">Section</li>
+				<li className="font-semibold">Section</li>
+				<li className="font-semibold">Section</li>
+				<li className="font-semibold">Section</li>
 			</ul>
 		</nav>
 	);
