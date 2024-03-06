4ae740627803461014f226cd35445d36c1940023
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-03-06 18:59:34 +0100

    🛠️ app: add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,43 @@
+export const data = [
+	{
+		id: 1,
+		date: "1885",
+		description: "Karl Benz construit la première voiture à moteur à essence",
+		isLeft: true,
+	},
+	{
+		id: 2,
+		date: "1908",
+		description:
+			"Henry Ford introduit la Ford Model T, la première voiture produite en plusieurs versions à grande échelle.",
+		isLeft: false,
+	},
+	{
+		id: 3,
+		date: "1930",
+		description:
+			"Début de la production en plusieurs voitures à carrosserie monocoque.",
+		isLeft: true,
+	},
+	{
+		id: 4,
+		date: "1954",
+		description:
+			"La première voiture équipée d'une transmission automatique, la Oldsmobile 98, est introduite.",
+		isLeft: false,
+	},
+	{
+		id: 5,
+		date: "1973",
+		description:
+			"Crise pétrolière qui entraîne une demande accrue de voitures économiques et économes en carburant.",
+		isLeft: true,
+	},
+	{
+		id: 6,
+		date: "1997",
+		description:
+			"Introduction de la Toyota Prius, la première voiture hybride grand public.",
+		isLeft: false,
+	},
+];
