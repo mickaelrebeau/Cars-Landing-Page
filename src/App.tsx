5116f3fcdb75039695487c64d2d72e16d2e991af
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-03-06 19:00:21 +0100

    🛠️ app: update app

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { background } from "./assets/videos";
 import { Gallery } from "./components/Gallery";
 import { Presentation } from "./components/Presentation";
 import { Zoom } from "./components/Zoom";
+import { History } from "./components/History";
 
 function App() {
 	return (
@@ -30,13 +31,14 @@ function Layout() {
 				<source src={background} type="video/mp4" />
 			</video>
 			<div className="w-full h-screen relative z-10">
-				<div className="top-0 left-1/2 -translate-x-1/2 mt-8 w-[80%] fixed bg-black/25 backdrop-blur-md rounded-full overflow-hidden">
+				<div className="top-0 left-1/2 -translate-x-1/2 mt-8 w-[80%] fixed bg-black/25 backdrop-blur-md rounded-full z-10 overflow-hidden">
 					<Header />
 				</div>
 				<Home />
 				<Presentation />
 				<Gallery />
 				<Zoom />
+				<History />
 			</div>
 		</div>
 	);
