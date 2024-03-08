c3aaaffd70a5fc3043cf4b6f84d092e9af019033
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-03-08 21:01:44 +0100

    🛠️ app: update history

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -5,18 +5,18 @@ import { slideIn } from "@/utils/motion";
 
 export const History = () => {
 	return (
-		<section className="w-full h-auto flex flex-col items-center justify-center gap-10">
-			<div className="pt-80 pb-24 w-[80%] h-full flex flex-col items-center justify-center gap-10 relative backdrop-blur-sm">
-				<span className="w-[1px] h-full text-8xl font-bold text-center bg-slate-400 rounded-full absolute top-0"></span>
-				<div className="w-full flex flex-col items-center gap-48 relative">
+		<section id="History" className="w-full h-auto flex flex-col items-center justify-center gap-10">
+			<div className="pt-80 pb-24 w-[80%] h-full flex flex-col items-center justify-center gap-10 relative backdrop-blur-md">
+				<span className="w-[1px] h-full text-8xl font-bold text-center bg-slate-600 rounded-full absolute top-0"></span>
+				<div className="w-full flex flex-col items-center gap-48 relative overflow-hidden">
 					{data.map((el) => {
 						return (
 							<motion.div
 								key={el.id}
 								variants={
 									el.isLeft
-										? slideIn("right", "tween", 0.2, 0.25)
-										: slideIn("left", "tween", 0.2, 0.25)
+										? slideIn("right", "tween", 0.35, 0.5)
+										: slideIn("left", "tween", 0.35, 0.5)
 								}
 								initial="hidden"
 								whileInView="show"
